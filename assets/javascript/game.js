ebe7e82723ac35e7eb981a807e57df3692116fd8
A.Sofiane Guerfi <asofianeguerfi@ASofianes-MacBook-Pro.local>
2017-11-09 17:14:21 -0800

    progress..

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+var word = ['colorful', 'festival', 
+			    'leaves', 'november', 
+				'season', 'cake', 'treat', 
+				'turkey', 'fall'];
+var randomword = word[Math.floor(Math.random() * word.length)];
+var count = 0;
+var wins = 0;
+var wordGuess = [];
+var wrongGuess = [];
+var underScore = [];
+var RemGuesses = 15;
